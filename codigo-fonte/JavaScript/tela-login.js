e45aa918ed7f1415179449534de34fa5784c2d05
Kevin Eduardo <kevineduardoferreira@gmail.com>
2024-11-24 00:11:20 -0300

    add feature "voltar" in "anunciar" window and fix error css in window "login"

--- codigo-fonte/JavaScript/tela-login.js
+++ codigo-fonte/JavaScript/tela-login.js
@@ -1,11 +1,12 @@
-function login() {
+const btnLogin = document.getElementById('button-login');
+
+btnLogin.addEventListener("click", function() {
     const username = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    // Validações básicas
-    if (username == "" || password == "") {
-        alert("Deverá ser preenchido todos os campos.")
-    } else{
-        window.location.href = "Shelf Skins 2.html"
+    if (username === "" || password === "") {
+        alert("Deverá ser preenchido todos os campos.");
+    } else {
+        window.location.href = "Shelf Skins 2.html";
     }
-}
+});
